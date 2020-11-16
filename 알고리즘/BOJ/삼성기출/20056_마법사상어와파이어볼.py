5599cb374278aad5ebf9534ac5b078c3713b8af0
Seungbum Park <parkbum11@naver.com>
2020-11-16 12:14:31 +0900

    vue

--- 알고리즘/BOJ/삼성기출/20056_마법사상어와파이어볼.py
+++ 알고리즘/BOJ/삼성기출/20056_마법사상어와파이어볼.py
@@ -1,64 +1,57 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-# 푼 시간 : 0849 -20m
-# 푼 날짜 : 2020.10.25
+# 푼 시간 : infini
+# 푼 날짜 : 2020.11.15
 
-# def separate(rr, cc):
-#     sum_m, sum_s, dd, cn = 0, 0, 0, len(arr[rr][cc])
-#     for i in range(len(arr[rr][cc])):
-#         m, s, d = arr[rr][cc].pop()
-#         sum_m += m
-#         sum_s += s
-#         dd += (d%2)
-#     sum_m //= 5
-#     sum_s //= cn
-#     if sum_m == 0:
-#         return
-#     if dd == 0 or dd == cn:
-#         dd = [0, 2, 4, 6]
-#     else:
-#         dd = [1, 3, 5, 7]
-#     for i in dd:
-#         r = rr + dr[i]
-#         c = cc + dc[i]
-#         if 0 <= r < N and 0 <= c < N and len(arr[r][c]) == 0:
-#             arr[r][c].append((sum_m, sum_s, i))
-#             move_info.append((r, c))
-#         elif 0 <= r < N and 0 <= c < N and len(arr[r][c]) != 0:
-#             arr[r][c].append((sum_m, sum_s, i))
-#             if (r, c) not in separate_info:
-#                 separate_info.append((r, c))
-# def move(r, c, m, s, d):
-#     rr = r + dr[d]*s
-#     cc = c + dc[d]*s
-#     arr[rr][cc].append((m, s, d))
-#     move_info.append((rr, cc))
-#     elif 0 <= rr < N and 0 <= cc < N and len(arr[rr][cc]) != 0:
-#         arr[rr][cc].append((m, s, d))
-#         if (rr, cc) not in separate_info:
-#             separate_info.append((rr, cc))
+# 매우 안좋은 코드라고 생각함.
+from collections import deque
 
-def move(r, c, m, s, d):
-    rr = r + dr[d]*s
-    cc = c + dc[d]*s
-    move_dumy.append(())
-
-
-move_dumy = []
-move_info = []
 dr = [-1, -1, 0, 1, 1, 1, 0, -1]
 dc = [0, 1, 1, 1, 0, -1, -1, -1]
 N, M, K = map(int, input().split())
-arr = [[0]*N for _ in range(N)]
+arr = [[[] for _ in range(N + 1)] for _ in range(N + 1)]
+order = 0
 for _ in range(M):
     r, c, m, s, d = map(int, input().split())
-    arr[r-1][c-1] += 1
-    move_info.append((r-1, c-1, m, s, d))
-while True:
-    ll = len(move_info)
-    for _ in range(ll):
-        r, c, m, s, d = move_info.pop(0)
-        if arr[r][c] == 1:
-            arr[r][c] -= 1
-            move(r, c, m, s, d)
+    arr[r][c].append((m, s, d, order))
+info = deque()
+while order != K:
+    seperate_arr = [[0] * (N + 1) for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            while len(arr[i][j]):
+                if arr[i][j][0][3] != order: break
+                else:
+                    m, s, d, num = arr[i][j].pop(0)
+                    rr, cc = i + dr[d] * s, j + dc[d] * s
+                    if rr <= 0 or rr > N or cc <= 0 or cc > N:
+                        rr, cc = (rr + N - 1) % N + 1, (cc + N - 1) % N + 1
+                    arr[rr][cc].append((m, s, d, num + 1))
+                    seperate_arr[rr][cc] += 1
+                    if seperate_arr[rr][cc] >= 2 and (rr, cc) not in info:
+                        info.append((rr, cc))
+    while len(info):
+        r, c = info.popleft()
+        check = len(arr[r][c])
+        new_m, new_s, new_d, new_num = 0, 0, 0, order + 1
+        while len(arr[r][c]):
+            m, s, d, num = arr[r][c].pop()
+            new_m += m
+            new_s += s
+            new_d += (d % 2)
+        new_m //= 5
+        if new_m == 0: continue
+        new_s //= check
+        if new_d == check or new_d == 0: ddd = 0
+        else: ddd = 1
+        for d in range(ddd, 8, 2):
+            arr[r][c].append((new_m, new_s, d, new_num))
+            ddd += 2
+    order += 1
+result = 0
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        for k in arr[i][j]:
+            result += k[0]
+print(result)
